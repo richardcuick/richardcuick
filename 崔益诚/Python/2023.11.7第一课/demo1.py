5df3b3bde27b8e6a8c7f1382f85ea567168aea1c
崔驰坤 Cui, Richard Chikun <richardcuick@outlook.com>
2023-11-23 08:42:33 +0800

    崔益诚代码更新

---
+++ 崔益诚/Python/2023.11.7第一课/demo1.py
@@ -0,0 +1,14 @@
+# 在这里写上你的代码 :-)
+"""
+print("姓名：崔益诚","   年龄：8","   身高：140","   学校：大连格致新校")
+print(1+3)
+print(4-2)
+print(7/3)
+print(7//3)
+print(7%3)
+print(2**3)
+"""
+# print("bitkid"*3)对
+# print("bitkid"+3)不对
+# print("bitkid"*"3")不对
+# print("bitkid"+"3")对
---
+++ 崔益诚/Python/2023.11.9第三课作业.py
@@ -0,0 +1,18 @@
+n=input("请输入您喜欢的图案:")
+row=input("请输入要打印的行数:")
+#print(" "*9+n*1)
+#print(" "*8+n*3)
+#print(" "*7+n*5)
+#print(" "*6+n*7)
+#print(" "*5+n*9)
+#print(" "*5+n*9)
+#print(" "*6+n*7)
+#print(" "*7+n*5)
+#print(" "*8+n*3)
+#print(" "*9+n*1)
+
+for i in range(0,int(row)):
+    print(" "*(int(row)-i-1)+n*(2*i+1))
+#print()
+for i in range(int(row)-1,0,-1):
+    print(" "*(int(row)-i)+n*(2*i-1))

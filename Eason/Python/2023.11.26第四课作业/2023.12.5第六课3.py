96bafb2636c37aba089a6ce772c43d8f394bc5ea
崔驰坤 Cui, Richard Chikun <richardcuick@outlook.com>
2026-08-26 14:56:15 +0800

    20260826-01

--- Eason/Python/2023.11.26第四课作业/2023.12.5第六课3.py
+++
@@ -1,7 +0,0 @@
-n=int(input())
-if 0<=n<=34:
-    print("优")
-elif 35<=n<=74:
-    print("良")
-elif 75<=n<=114:
-    print("轻")
--- Eason/Python学习.md
+++
@@ -1,1 +0,0 @@
-* Python IDE下载地址: [MU Editor](https://codewith.mu/en/download) - 最新版本1.2.0
